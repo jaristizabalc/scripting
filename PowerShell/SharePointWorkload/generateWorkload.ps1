2aca6a9d1276f75c75ba360f97755b611be733ee
Juan Aristizabal <jaristizabalc@gmail.com>
2018-09-20 13:55:19 -0600

    Update generateWorkload.ps1

--- PowerShell/SharePointWorkload/generateWorkload.ps1
+++ PowerShell/SharePointWorkload/generateWorkload.ps1
@@ -16,10 +16,11 @@ function getTrendingTag(){
 }
 function pushGfycatFixedSize($downloadFolder,$targetSize){
     #Prep download folder
-    if (Test-Path $downloadFolder) {
-        Remove-Item ((Get-Location).Path+"\"+$downloadFolder) -Force -Recurse -ErrorAction SilentlyContinue
+    $location = ("C:\Users\Administrator\Documents\"+$downloadFolder)
+    if (Test-Path $location) {
+        Remove-Item $location -Force -Recurse -ErrorAction SilentlyContinue
     }
-    New-Item $downloadFolder -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+    New-Item $location -ItemType Directory -ErrorAction SilentlyContinue
     #limit image size
     $limitSize = 100*1024*1024 #max is 100 MB
     $totalSize = 0
@@ -40,7 +41,9 @@ function pushGfycatFixedSize($downloadFolder,$targetSize){
             if($gfyCat.gifSize -ge $limitSize) {Write-Host "Image too big, skipping"}
             else {
                 $totalSize = $totalSize + $gfyCat.gifSize
-                $outfile =  (Get-Location).Path + "\" + $downloadFolder + "\" + $gfyCat.gfyId + ".gif"
+                #add a unique timestamp to the filename so duplicates make it to the database, otherwise they will be overwritten without increasing the DB size (but will count as block changes from the SRN point of view)
+                $unixTimeStamp = [math]::floor((Get-Date -UFormat %s))
+                $outfile =  $location + "\" + $gfyCat.gfyId + "_" + $unixTimeStamp + ".gif"
                 Write-Host "Downloading $($gfyCat.gfyId).gif, ->$tag ,"
                 Invoke-WebRequest -Uri $gfyCat.gifUrl -OutFile $outfile
                 if($totalSize -ge $targetSize) {break}
@@ -63,48 +66,121 @@ $userArray = "user1","user2","user3","user4","user5"				#add more users by addin
 $password = "P@ssw0rd"
 #Logfile
 $date = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
-$logFile = "GFW_"+$date+".csv"
+$logFile =  "C:\Users\Administrator\Documents\GFW_"+$date+".csv"
 New-Item $logFile -value "time,user,transferredBytes`r`n" -type file -Force
 $downloadFolder = "gfyDownloads"
 #Calculate the targetsize for downloads and the sleep time based on the desired change rate
 #Set desired data change in a day (bytes)
-$dailyChange = 1024*1024*1024*10  #10GB
+$dailyGB = 48                     #target Download size per day in GB
+$dailyChange = (1024*1024*1024)*$dailyGB 
 $factor = 2                       #use it to tune the frequency of uploads, 1 is each hour, 2 is every half an hour, 4 every 15 minutes etc....
 $frequency = 24*$factor                 #upload files half and hour
 $sleepTime = 60*60/$factor               #
 $targetSize = $dailyChange/$frequency
 Write-Host "Download target size is $targetSize bytes every $sleepTime seconds ,"
-
+$uploadCounter = 0                 #record the bytes uploaded so cleanup can be triggered accordingly
 while($true) {
-    $rnd = Get-Random -Minimum 0 -Maximum $userArray.Length
-    $username = $userArray[$rnd]
-    Write-Host "Start downloading gfycats for user $username ,"
-    $StartPoint=(Get-Date)
-    Write-Host $StartPoint
-    pushGfycatFixedSize $downloadFolder $targetSize
-    #Upload the images to SharePoint
-    $securePasssword = ConvertTo-SecureString $password -AsPlainText -Force 
-    $credentials = New-Object System.Management.Automation.PSCredential ($username, $securePasssword)
-    $webclient = New-Object System.Net.WebClient
-    $webclient.Credentials = $credentials
-    $files = get-childitem ((Get-Location).Path+"\"+$downloadFolder)
-    $uploadSize = 0
-    foreach($file in $files) {
+    if($uploadCounter -lt $dailyChange)
+    {
+        $rnd = Get-Random -Minimum 0 -Maximum $userArray.Length
+        $username = $userArray[$rnd]
+        Write-Host "Start downloading gfycats for user $username ,"
+        $StartPoint=(Get-Date)
+        Write-Host "Starting cycle at $StartPoint"
+        pushGfycatFixedSize $downloadFolder $targetSize
+        Write-Host "Finished downloading at $(Get-Date)"
+        #Upload the images to SharePoint
+        $securePasssword = ConvertTo-SecureString $password -AsPlainText -Force 
+        $credentials = New-Object System.Management.Automation.PSCredential ($username, $securePasssword)
+        $webclient = New-Object System.Net.WebClient
+        $webclient.Credentials = $credentials
+        $files = get-childitem ("C:\Users\Administrator\Documents\"+$downloadFolder)
+        $uploadSize = 0
+        $pass = 0
+        foreach($file in $files) {
         Write-Host "Uploading $($file.Name) ,"
-        $webclient.UploadFile($spHost + "/" + $file.Name, "PUT", $file.FullName)
-        $uploadSize = $uploadSize + $file.Length
+        try 
+        {
+            [byte[]] $response = $webclient.UploadFile($spHost + "/" + $file.Name, "PUT", $file.FullName)
+            Write-Host $response
+            $uploadSize = $uploadSize + $file.Length
+            $pass = $pass + 1
+        }
+        catch [System.Net.WebException]
+        {
+            Write-Host "Upload failed"
+
+        }
+        
     }
-    #Write to logfile
-    $date = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
-    Add-Content $logFile ($date+","+$username+","+$uploadSize)
-    $EndPoint=(Get-Date)
-    Write-Host $EndPoint
-    $processTime = NEW-TIMESPAN –Start $StartPoint –End $EndPoint
-    Write-host "Difference in date: $processTime"
-    $waitTime = $sleepTime - ($processTime.Hours*60*60 + $processTime.Minutes*60 + $processTime.Seconds)
-    Write-Host "Difference in seconds: $waitTime"
-    if($waitTime -gt 0){
-        Write-Host "Sleeping for $waitTime seconds ,"
-        Start-Sleep $sleepTime
+        Write-Host "Uploaded $pass files"
+        #Write to logfile
+        $date = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+        Add-Content $logFile ($date+","+$username+","+$uploadSize)
+        $uploadCounter = $uploadCounter + $uploadSize
+        $EndPoint=(Get-Date)
+        Write-Host "Finished cycle at $EndPoint"
+        $differenceTime = NEW-TIMESPAN –Start $StartPoint –End $EndPoint
+        Write-Host "Time difference is $differenceTime"
+        $taskTime = $differenceTime.Hours*60*60 + $differenceTime.Minutes*60 + $differenceTime.Seconds
+        $waitTime = $sleepTime - $taskTime
+        Write-Host "Sleeping $sleepTime  - $taskTime"
+        if($waitTime -gt 0){
+            Write-Host "Sleeping for $waitTime seconds ,"
+            Start-Sleep $waitTime
+        }
+        else {Write-Host "Negative difference, no sleeping"}
+    }
+    else {
+        #clean-up files on SharePoint
+        Write-Host "Cleanup SharePoint Library to avoid filling up file system, target is $dailyGB GB"
+        #Record process on logfile
+        $date = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+        $sharePointAdmin = "username"
+        $sharePointIP = "A.B.C.D"    #Update Value
+        Add-Content $logFile ($date+",StartCleanup,-----------------------------")
+        $pwd = Get-Content c:\Users\Administrator\Documents\sp_creds.txt | ConvertTo-SecureString
+        $creds = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $sharePointAdmin,$pwd 
+        Invoke-Command -ComputerName $sharePointIP -Credential $creds -Authentication CredSSP -ArgumentList $dailyGB -ScriptBlock {
+            #Commands to execute on SharePoint server
+            Add-PSSnapin Microsoft.SharePoint.PowerShell
+            $web = Get-SPWeb -Identity "http://SharePointServer/sites/SiteName"  #Update values
+            $list = $web.GetList("http://SharePointServer/sites/SiteName/Downloads")    #Update Values   
+            $folderUrl = $list.RootFolder.Url
+            $folder = $web.GetFolder($folderUrl)
+            $deleteMax = [int]$($args[0])*1024*1024*1024
+            $total = 0
+            $items = 0
+            foreach ($file in $folder.Files) {
+	            $total = $total + $file.Length
+	            $items = $items+1
+            }
+            $totalGB = ([math]::Round($total/(1024*1024*1024), 4))
+            Write-Host "Currently $items items on Library, for a total of $totalGB GB, deleting $deleteMax bytes ..."
+            $current = 0 
+            foreach ($file in $folder.Files) {
+	            if($current -le $deleteMax){
+		            #Write-Host("DELETED FILE: " + $file.name)
+        	        $list.Items.DeleteItemById($file.Item.Id)
+		            $current = $current + $file.Length	
+	            }
+	            else {
+		            Write-Host "Deleted $current bytes already, exiting loop"
+		            break
+	            }
+            }
+            $folder = $web.GetFolder($folderUrl)
+            $total = 0
+            $items = 0
+            foreach ($file in $folder.Files) {
+	            $total = $total + $file.Length
+	            $items = $items+1
+            }
+            $totalGB = ([math]::Round($total/(1024*1024*1024), 4))
+            Write-Host "$items items on Library, for a total of $totalGB GB"
+        }
+        $uploadCounter = 0
+        $date = Get-Date -Format "MM-dd-yyyy_hh-mm-ss"
+        Add-Content $logFile ($date+",FinishCleanup,---------------------------")
     }
 }
